2615f381a0bd8eb3237f030e0647b6273f6f4bdc
slowdh <leok0909@snu.ac.kr>
2023-04-11 15:49:38 +0900

    add basic message response

--- bot/slack_bot.py
+++ bot/slack_bot.py
@@ -17,8 +17,18 @@ app = Flask(__name__)
 slack_event_adapter = SlackEventAdapter(SIGNING_SECRET, '/slack/events', app)
 
 client = slack.WebClient(token=SLACK_TOKEN)
+BOT_ID = client.api_call("auth.test")['user_id']
 
-client.chat_postMessage(channel='#dev', text="Hello world!")
+@slack_event_adapter.on('message')
+def handle_message(payload):
+    event = payload.get('event', {})
+    user_id = event.get('user')
+    channel_id = event.get('channel')
+    text = event.get('text')
+
+    if BOT_ID != user_id:
+        client.chat_postMessage(channel=channel_id, text=text)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5023)
+ 
