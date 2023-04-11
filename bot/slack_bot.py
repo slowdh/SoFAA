b145d137caa68e8aedbd7054d0dab6b2f2c4fd34
slowdh <leok0909@snu.ac.kr>
2023-04-11 14:35:02 +0900

    add flask server

--- bot/slack_bot.py
+++ bot/slack_bot.py
@@ -2,10 +2,23 @@ import os
 
 import slack
 from dotenv import load_dotenv
+from flask import Flask
+from slackeventsapi import SlackEventAdapter
 
+
+# run local tunnel first
+# https://slackbotforsofaa2023.loca.lt
 
 load_dotenv()
+SLACK_TOKEN = os.environ['SLACK_TOKEN']
+SIGNING_SECRET = os.environ['SIGNING_SECRET']
 
-client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
+app = Flask(__name__)
+slack_event_adapter = SlackEventAdapter(SIGNING_SECRET, '/slack/events', app)
+
+client = slack.WebClient(token=SLACK_TOKEN)
 
 client.chat_postMessage(channel='#dev', text="Hello world!")
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5023)
--- bot/things_to_do.md
+++ bot/things_to_do.md
@@ -3,3 +3,7 @@
     - go to slack webpage, set scope, get token
 
 - add bot to channel
+
+
+#### tunneling
+- use localtunnel
