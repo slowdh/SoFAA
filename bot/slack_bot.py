872c3fb5dd1e20260e05d4187989bef2ef43785e
slowdh <leok0909@snu.ac.kr>
2023-04-11 19:32:58 +0900

    update how to use

--- bot/slack_bot.py
+++ bot/slack_bot.py
@@ -32,12 +32,16 @@ def get_welcome_message():
         'type': 'mrkdwn',
         'text': (
                 ':bubbles: *SoFAA에 오신 것을 환영합니다!* \n\n\n'
-                '다음 명령어를 이용해서 디자인을 진행해보세요. (현재는 영어만 지원하고 있습니다.) \n\n\n'
+                'SoFAA는 현재 영어만 지원하고 있으며, 디자인은 #design 채널에서 생성 가능합니다. \n\n\n\n\n'
+                '다음 명령어를 이용해서 디자인을 진행해보세요. \n\n\n'
                 ':classical_building: */design 명령어와 --키워드1 --키워드2 --키워드3 ... 방식으로 디자인을 생성해보세요.* \n\n\n'
                 'ex) /design --organic --futuristic --zaha hadid --sunset\n\n\n'
                 ':classical_building: */develop 명령어와 --image_id 입력을 통해서 생성된 디자인을 디벨롭해보세요.* \n\n\n'
-                'develop을 먼저 진행하시면 id가 이미지와 함께 제공됩니다. \n\n\n'
-                'ex) /develop --id0123984213'
+                'ex) /develop --id0123984213 \n\n\n'
+                '(develop을 먼저 진행하시면 id가 이미지와 함께 제공됩니다.) \n\n\n\n\n'
+                '각 명령어는 4장의 이미지를 생성합니다. \n\n\n'
+                '생성 속도가 느리다면 조금만 기다려주세요. 개인 서버로 운영되고 있어서 처리 속도가 느릴 수 있습니다. \n\n\n'
+                '문제가 생길 시 @dev_SoFAA 로 DM을 주시면 처리 도와드리겠습니다.'
             )
         }
     }]
@@ -63,7 +67,7 @@ def handle_explain_message(payload):
     if text == '!설명':
         welcome_msg = get_welcome_message()
         client.chat_postMessage(channel=f'@{user_id}', blocks=welcome_msg)
-        client.chat_postMessage(channel=channel_id, text=f":bubbles: SoFAA 사용법을 DM으로 보내드렸습니다.")
+        client.chat_postMessage(channel=channel_id, text=f":bubbles: <@{user_id}>님 SoFAA 사용법을 DM으로 보내드렸습니다.")
 
 
 @app.route('/design', methods=['POST'])
