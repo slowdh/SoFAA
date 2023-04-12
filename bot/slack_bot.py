61405fb45b0feb5fc3486c5fd9e0e01c8087115f
slowdh <leok0909@snu.ac.kr>
2023-04-12 21:50:35 +0900

    update welcome message

--- bot/slack_bot.py
+++ bot/slack_bot.py
@@ -74,16 +74,17 @@ def get_welcome_message():
     'text': {
         'type': 'mrkdwn',
         'text': (
-                ':bubbles: *SoFAA에 오신 것을 환영합니다!* \n\n\n'
-                'SoFAA는 현재 영어만 지원하고 있으며, 디자인은 #design 채널에서 생성 가능합니다. \n\n\n\n\n'
-                '#design 채널에서 다음 명령어를 입력해서 디자인을 진행해보세요. \n\n\n'
-                ':art: */design 명령어와 --키워드1 --키워드2 --키워드3 ... 방식으로 디자인을 생성해보세요.* \n\n\n'
-                'ex) /design --organic --futuristic --zaha hadid --sunset\n\n\n'
-                ':building_construction: */develop 명령어와 --image_id 입력을 통해서 생성된 디자인을 디벨롭해보세요.* \n\n\n'
-                'ex) /develop --id0123984213 \n\n\n'
-                '(develop을 먼저 진행하시면 id가 이미지와 함께 제공됩니다.) \n\n\n\n\n'
-                '각 명령어는 4장의 이미지를 생성합니다. \n\n\n'
-                '생성 속도가 느리다면 조금만 기다려주세요. 개인 서버로 운영되고 있어서 처리 속도가 느릴 수 있습니다. \n\n\n'
+                ':bubbles: *SoFAA에 오신 것을 환영합니다!* \n\n'
+                'SoFAA는 현재 영어만 지원하고 있으며, 디자인은 #design 채널에서 생성 가능합니다. \n\n'
+                '#design 채널에서 다음 명령어를 입력해서 디자인을 진행해보세요. \n\n'
+                ':art: */design 명령어와 --키워드1 --키워드2 --키워드3 ... 방식으로 디자인을 생성해보세요.* \n\n'
+                '키워드 간 띄어쓰기가 가능하고 구체적인 설명 또한 입력 가능합니다\n\n'
+                'ex) /design --organic --futuristic --like zaha hadid --concrete material \n\n'
+                ':building_construction: */develop 명령어와 --image_id 입력을 통해서 생성된 디자인을 디벨롭해보세요.* \n\n'
+                'develop을 먼저 진행하시면 id가 이미지와 함께 제공됩니다. \n\n'
+                'ex) /develop --id0123984213 \n\n'
+                '각 명령어는 4장의 이미지를 생성합니다. \n\n'
+                '생성 속도가 느리다면 조금만 기다려주세요. 개인 서버로 운영되고 있어서 처리 속도가 느릴 수 있습니다. \n\n'
                 '문제가 생길 시 @dev_SoFAA 로 DM을 주시면 처리 도와드리겠습니다.'
             )
         }
@@ -122,7 +123,7 @@ def handle_design():
     channel_id = data.get('channel_id')
     text = data.get('text')
     prompt = text.strip("--").replace("--", ', ')
-    
+
     chat_response = client.chat_postMessage(
         channel=channel_id, 
         text=f":bubbles: Designing :: [{text}] | {task_queue.qsize()} design waiting..."
